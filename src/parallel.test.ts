6b1d7a714d94b08cfd8757f4fa4350cb7dc5a536
JakeBeaver <jakub@kolezyn.ski>
2023-01-14 22:16:11 +0000

    remove unnecessary files

--- src/parallel.test.ts
+++
@@ -1,7 +0,0 @@
-import { it, expect } from "vitest";
-import { getNumberOfContainers, spawnContainer } from ".";
-it("fresh module per file", () => {
-  expect(getNumberOfContainers()).toBe(0);
-  spawnContainer();
-  expect(getNumberOfContainers()).toBe(1);
-});
