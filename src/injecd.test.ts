c0af7882d2836e267ee7b7a9618a0de027bca800
JakeBeaver <jakub@kolezyn.ski>
2023-01-14 10:36:40 +0000

    tests

--- src/injecd.test.ts
+++ src/injecd.test.ts
@@ -1,11 +1,59 @@
-import { Container, Injectable } from "./injecd";
+import { spawnContainer, injecd, injecdByReturn } from "./injecd";
 import { it, expect } from "vitest";
 
-it("basic register resolve", () => {
-  const str = new Injectable<string>();
-  const container = new Container();
+it.fails(".r throws outside container", () => injecd<string>().r);
+it.fails(".option throws outside container", () => injecd<string>().option);
+it.fails(".or() throws outside container", () =>
+  injecd<string>().or("test")
+);
 
-  container.registerInstance(str, "testing");
+it("basic resolve", () => {
+  const stringT = injecd<string>();
+  const container = spawnContainer();
 
-  expect(str.r).toBe("testing");
+  container.registerInstance(stringT, "testing");
+  const resolved = container.resolve(stringT);
+
+  expect(resolved).toBe("testing");
+});
+
+it("nested resolve", () => {
+  const nestedStringT = injecd<string>();
+  const parentFuncT = injecdByReturn(parentFuncFactory);
+  function parentFuncFactory(a: string = nestedStringT.r): () => string {
+    return () => a;
+  }
+  const container = spawnContainer();
+  container.registerInstance(nestedStringT, "a");
+  container.registerFactory(parentFuncT, parentFuncFactory);
+
+  const resolvedFunc = container.resolve(parentFuncT);
+  expect(resolvedFunc()).toBe("a");
+});
+
+it(".or() without injection gives default value", () => {
+  const nestedStringT = injecd<string>();
+  const parentFuncT = injecdByReturn(parentFuncFactory);
+  function parentFuncFactory(a: string = nestedStringT.or("default value")): () => string {
+    return () => a;
+  }
+  const container = spawnContainer();
+  container.registerFactory(parentFuncT, parentFuncFactory);
+
+  const resolvedFunc = container.resolve(parentFuncT);
+  expect(resolvedFunc()).toBe("default value");
+});
+
+it(".option without injection gives default value", () => {
+  const nestedStringT = injecd<string>();
+  const parentFuncT = injecdByReturn(parentFuncFactory);
+  function parentFuncFactory(a = nestedStringT.option): () => string | undefined {
+    return () => a;
+  }
+  
+  const container = spawnContainer();
+  container.registerFactory(parentFuncT, parentFuncFactory);
+
+  const resolvedFunc = container.resolve(parentFuncT);
+  expect(resolvedFunc()).toBeUndefined();
 });
--- src/injecd.ts
+++ src/injecd.ts
@@ -1,47 +1,82 @@
 let idCounter = 1;
-let currentContainerId = 0;
-export class Container {
+let currentContainerId: number | null = null;
+const error = <T>(message: string): T => {
+  throw message;
+};
+const getContainer = (): number =>
+  currentContainerId ||
+  error("injecd container action outside of container scope");
+
+class Container {
   private id: number;
-  private activate = () => (currentContainerId = this.id);
+  private scope<T>(action: () => T) {
+    currentContainerId = this.id;
+    const output = action();
+    currentContainerId = null;
+    return output;
+  }
   public static get current() {
-    if (!currentContainerId) throw "Some container must be initialized";
-    return currentContainerId;
+    return currentContainerId || error("Some container must be initialized");
   }
   constructor() {
     this.id = idCounter++;
   }
   public registerInstance<T>(resolver: Injectable<T>, instance: T) {
-    this.activate();
-    resolver.DO_NOT_USE_registerInstance(instance);
+    this.scope(() => resolver.internals.registerInstance(instance));
   }
   public registerFactory<T>(resolver: Injectable<T>, factory: () => T) {
-    this.activate();
-    resolver.DO_NOT_USE_registerFactory(factory);
+    this.scope(() => resolver.internals.registerFactory(factory));
   }
   public resolve<T>(resolver: Injectable<T>) {
-    this.activate();
-    return resolver.resolve();
+    return this.scope(() => resolver.internals.resolveHard());
   }
   public resolveFunc<T>(func: () => T) {
-    this.activate();
-    return func();
+    return this.scope(() => func());
   }
 }
 
-export class Injectable<T> {
-  private containers = new Map<number, () => T>();
-  DO_NOT_USE_registerFactory(getter: () => T) {
-    this.containers.set(currentContainerId, getter);
-  }
-  DO_NOT_USE_registerInstance(instance: T) {
-    this.containers.set(currentContainerId, () => instance);
-  }
-  resolve(): T {
-    const getter = this.containers.get(currentContainerId);
-    if (!getter) throw "resolver must be registered!";
-    return getter();
-  }
+class Injectable<T> {
+  public internals = new InjectableInternals<T>();
   get r() {
-    return this.resolve();
+    return this.internals.resolveHard();
+  }
+  get option() {
+    return this.internals.resolveSoft();
+  }
+  or(defaultValue: T): T {
+    return this.internals.resolveSoft() || defaultValue;
   }
 }
+
+class InjectableInternals<T> {
+  private containers = new Map<number, () => T>();
+  private getFactory(): (() => T) | undefined {
+    return this.containers.get(getContainer());
+  }
+  private setFactory(getter: () => T) {
+    this.containers.set(getContainer(), getter);
+  }
+  registerFactory(getter: () => T) {
+    this.setFactory(getter);
+  }
+  registerInstance(instance: T) {
+    this.setFactory(() => instance);
+  }
+  resolveHard(): T {
+    const get = this.getFactory();
+    if (!get) throw "tried to resolve an unregistered injectable";
+    return get();
+  }
+  resolveSoft(): T | undefined {
+    const get = this.getFactory();
+    if (!get) return undefined;
+    return get();
+  }
+}
+
+export const spawnContainer = () => new Container();
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const injecd = <T>(dummy?: T) => new Injectable<T>();
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const injecdByReturn = <T extends () => unknown>(dummyFactory?: T) =>
+  new Injectable<ReturnType<T>>();
